3979a70957fcc722463239917d851d27ac5d656d
Bradley Matera <162147754+BradleyMatera@users.noreply.github.com>
2026-08-03 20:22:36 -0500

    Add minimal accessible interaction script

---
+++ assets/site.js
@@ -0,0 +1,10 @@
+(() => {
+  document.querySelectorAll('details').forEach((item) => {
+    item.addEventListener('toggle', () => {
+      if (!item.open) return;
+      document.querySelectorAll('details[open]').forEach((other) => {
+        if (other !== item) other.open = false;
+      });
+    });
+  });
+})();
